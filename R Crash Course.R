3ade7cdbba79cc8266a575310501e5945a8f7273
ChiobiJason <chisomchiobi@gmail.com>
2025-04-29 07:21:50 -0600

    Learnt Cleaning of Data

--- R Crash Course.R
+++ R Crash Course.R
@@ -87,3 +87,61 @@ starwars %>%
 starwars %>% 
   select(name, mass, height, everything()) %>% 
   View()
+
+# Changing variable names
+starwars %>% 
+  rename("character" = "name") %>%
+  head()
+
+# Changing a variable type
+class(starwars$hair_color)
+
+starwars$hair_color <- as.factor(starwars$hair_color)
+
+class(starwars$hair_color)
+
+starwars %>% 
+  mutate(hair_color = as.character(hair_color)) %>% 
+  glimpse()
+
+# Changing factor levels
+df <- starwars
+df$sex <- as.factor(df$sex)
+
+levels(df$sex)
+
+df <- df %>% 
+  mutate(sex = factor(sex, 
+                      levels = c("male", "female", "hermaphroditic", "none"
+                                 )))
+
+levels(df$sex)
+
+# Filter rows
+starwars %>% 
+  select(mass, sex) %>% 
+  filter(mass < 55 &
+           sex == "male")
+
+# Recode data
+starwars %>%
+  select(sex) %>% 
+  mutate(sex = recode(sex,
+                      "male" = "man",
+                      "female" = "woman"))
+
+# Dealing with missing data
+mean(starwars$height, na.rm = TRUE)
+
+# Dealing with duplicates
+Names <- c("Peter", "John", "Andrew", "Peter")
+Age <- c(22, 33, 44, 22)
+
+friends2 <- data.frame(Names, Age)
+
+friends2
+
+friends2 %>% 
+  distinct()
+
+distinct(friends2)
