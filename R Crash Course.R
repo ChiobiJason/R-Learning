9238482420f7996f95253c0bb31ddbcafe235be2
ChiobiJason <chisomchiobi@gmail.com>
2025-04-29 21:41:35 -0600

    Finished learning Data Visualization

--- R Crash Course.R
+++ R Crash Course.R
@@ -226,3 +226,62 @@ msleep %>%
   select(vore, order) %>% 
   filter(order %in% c("Rodentia", "Primates")) %>% 
   table()
+
+
+# Visualise
+#################
+
+plot(pressure)
+
+# The grammar of graphics
+    # data
+    # mapping
+    # geometry
+
+# Bar plots
+ggplot(data = starwars,
+       mapping = aes(x = gender))+
+  geom_bar()
+
+# Histograms
+starwars %>%
+  drop_na(height) %>% 
+  ggplot(mapping = aes(x = height))+
+  geom_histogram()
+ 
+# Box plots
+starwars %>% 
+  drop_na(height) %>% 
+  ggplot(aes(height))+
+  geom_boxplot(fill = "steelblue")+
+  theme_bw()+
+  labs(title = "Boxplot of height",
+       x = "Height of characters")
+
+# Density plots
+starwars %>%
+  drop_na(height) %>%
+  filter(sex %in% c("male", "female")) %>%
+  ggplot(mapping = aes(x = height,
+                       color = sex,
+                       fill = sex))+
+  geom_density(alpha = 0.2)+
+  theme_bw()
+
+# Scatter plots
+starwars %>% 
+  filter(mass < 200) %>%
+  ggplot(aes(height, mass, colour = sex))+
+  geom_point(size = 5, alpha = 0.5)+
+  theme_minimal()+
+  labs(title = "Height and mass by sex")
+
+# Smoothed model
+starwars %>% 
+  filter(mass < 200) %>% 
+  ggplot(aes(height, mass, color = sex))+
+  geom_point(size = 3, alpha = 0.8)+
+  geom_smooth()+
+  facet_wrap(~sex)+
+  theme_bw()+
+  labs(title = "Height and mass by sex")
