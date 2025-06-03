274b67d0684d9a3e41e3a4ba6a22737348ec56b4
ChiobiJason <chisomchiobi@gmail.com>
2025-06-03 14:56:49 -0600

    Exploring map visualizations

---
+++ Map Visualizations/Map Plots.R
@@ -0,0 +1,82 @@
+library(ggplot2)
+
+ggplot() +
+    borders("world", region = "Canada") +
+    coord_fixed()
+
+library(sf)
+library(rnaturalearth)
+library(ggplot2)
+
+canada <- ne_states(country = "canada", returnclass = "sf")
+
+ggplot(canada) +
+    geom_sf()
+
+
+library(canadamaps)
+library(ggplot2)
+
+ggplot(canada_prov_sf) +
+    geom_sf()
+
+
+library(choroplethr)
+library(choroplethrMaps)
+
+data(canada.map)
+
+canada_choropleth(df = your_data_frame)
+
+
+library(leaflet)
+
+leaflet() %>%
+    addTiles() %>%
+    setView(lng = -100, lat = 60, zoom = 3)
+
+
+#######################################################################
+library(sf)
+library(ggplot2)
+library(rnaturalearth)
+library(dplyr)
+
+# Get Canadian provinces
+canada <- ne_states(country = "Canada", returnclass = "sf")
+
+class(df)
+
+df <- data.frame(
+    name = c("Quebec", "Ontario", "Alberta", "British Columbia"),
+    value = c(23, 45, 12, 30)
+)
+
+# Join with your data (make sure `name` matches)
+canada_data <- canada %>%
+    left_join(df, by = "name")
+
+# Plot
+ggplot(canada_data) +
+    geom_sf(aes(fill = value)) +
+    scale_fill_viridis_c(option = "C") +
+    labs(title = "Example Choropleth of Canadian Provinces", fill = "Value") +
+    theme_minimal()
+######################################################################################
+
+# WEB BASED DASHBOARDS (Shiny)
+library(leaflet)
+
+leaflet() %>%
+    addTiles() %>%
+    setView(lng = -100, lat = 60, zoom = 3)
+
+
+
+library(tmap)
+data("World")
+
+tm_shape(World) +
+    tm_borders() +
+    tm_fill("continent")
+
